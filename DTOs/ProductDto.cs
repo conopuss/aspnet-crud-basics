f74ae5ab369a77a13de6eca64ee0170986ab9ba8
irmak <irmak.sabuncu@yahoo.com.tr>
2026-02-04 11:45:22 +0000

    Add Product DTO

---
+++ DTOs/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP_NET_Practice_withDB.DTOs
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Product name is required")]
+        [StringLength(50, MinimumLength =2)]
+        public string Name { get; set; }
+
+        [Range(0,200000)]
+        public decimal SalePrice { get; set; }
+    }
+}
