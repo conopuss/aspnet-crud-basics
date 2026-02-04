ac4a0526b7352be007548536f98ca3240b1a79f1
irmak <irmak.sabuncu@yahoo.com.tr>
2026-02-04 11:54:49 +0000

    Add product view model

---
+++ ViewModel/ProductModelView.cs
@@ -0,0 +1,12 @@
+﻿using ASP_NET_Practice_withDB.DTOs;
+
+namespace ASP_NET_Practice_withDB.ViewModel
+{
+    public class ProductModelView
+    {
+        public List<ProductDto> OrderProducts { get; set; }
+        public List<ProductDto> ExpensiveProducts { get; set; }
+        public ProductDto MostExpensive {  get; set; }  
+        public int ProductCount { get; set; }
+    }
+}
