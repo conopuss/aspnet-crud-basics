be75281fef0dad7a84f109961822a8d8ed58271d
irmak <irmak.sabuncu@yahoo.com.tr>
2026-02-04 11:49:47 +0000

    Add product repository interface

---
+++ Repositories/IProductRespository.cs
@@ -0,0 +1,12 @@
+﻿using ASP_NET_Practice_withDB.Models;
+namespace ASP_NET_Practice_withDB.Repositories
+{
+    public interface IProductRespository
+    {
+        List<Product> GetAll();
+        void Add(Product product);
+        Product GetID(int id);
+        void Edit(Product product);
+        void Delete(Product product);   
+    }
+}
