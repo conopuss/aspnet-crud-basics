7701ba0db4867f6c7c1654983b80b267d8b9a2d9
irmak <irmak.sabuncu@yahoo.com.tr>
2026-02-04 11:52:28 +0000

    Add product service interface

---
+++ Services/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using ASP_NET_Practice_withDB.DTOs;
+namespace ASP_NET_Practice_withDB.Services
+{
+    public interface IProductService
+    {
+        List<ProductDto> GetProductDtos();
+        void Create(ProductDto productDto);
+        ProductDto GetID_Rep(int id);
+        void Update(ProductDto productDto);
+        void Delete(int id);
+
+        List<ProductDto> OrderProducts();
+        List<ProductDto> ExpensiveProducts();
+        ProductDto MostExpensive();
+        int ProductCount();
+    }
+}
