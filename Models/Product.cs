7a54351477e323b9d74871f735f68eda0c716a41
irmak <irmak.sabuncu@yahoo.com.tr>
2026-02-04 11:43:49 +0000

    Add Product domain model

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP_NET_Practice_withDB.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal CostPrice { get; set; }
+        public decimal SalePrice { get; set; }
+    }
+}
