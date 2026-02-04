69327cca5c2a7d4477bb54adb155037d80493dba
irmak <irmak.sabuncu@yahoo.com.tr>
2026-02-04 11:47:07 +0000

    Add database connection layer

---
+++ Data/ConnectDB.cs
@@ -0,0 +1,13 @@
+﻿using ASP_NET_Practice_withDB.Models;
+using Microsoft.EntityFrameworkCore;
+namespace ASP_NET_Practice_withDB.Data
+{
+    public class ConnectDB:DbContext
+    {
+        public ConnectDB(DbContextOptions<ConnectDB>options):base(options) 
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
