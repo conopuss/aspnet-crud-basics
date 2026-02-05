b060bb0557f72b63f9062619249ae4b40586e802
irmak <irmak.sabuncu@yahoo.com.tr>
2026-02-05 11:04:30 +0000

    Add API controller

---
+++ Controllers/APIController.cs
@@ -0,0 +1,23 @@
+﻿using ASP_NET_Practice_withDB.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Practice_withDB.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class APIController : ControllerBase
+    {
+        private readonly IProductService _service;
+        public APIController(IProductService service)
+        {
+            _service = service;
+        }
+        public IActionResult Index()
+        {
+            var products = _service.GetProductDtos();
+            return Ok(products);
+        }
+    }
+}
